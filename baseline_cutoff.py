fa9addef232350ab668334c4e6af755af9a21aa3
Jonas Powell <jonasori@gmail.com>
2018-07-02 11:39:26 -0400

    Changed baseline range.

--- baseline_cutoff.py
+++ baseline_cutoff.py
@@ -11,7 +11,7 @@ import pandas as pd
 from tools import icr, imstat
 
 # baselines = [i*20 for i in range(1, 5)]
-baselines = np.arange(40, 150, 5)
+baselines = np.arange(0, 150, 5)
 dfile = 'data-hco'
 
 
