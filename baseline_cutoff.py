4f47a5c4ee3b38fb01b19ee60bdb9188e86c7c11
Jonas Powell <jonasori@gmail.com>
2018-07-02 11:08:19 -0400

    Added a check in get_baseline_rmss to see if convolved image already exists.

--- baseline_cutoff.py
+++ baseline_cutoff.py
@@ -6,7 +6,7 @@ Testing a change.
 import subprocess as sp
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
+import argparse as ap
 import pandas as pd
 from tools import icr
 from tools import imstat
@@ -16,41 +16,47 @@ baselines = np.arange(40, 150, 5)
 dfile = 'data-hco'
 
 
-
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='''Make a run happen.''')
+    """Run it."""
+    parser = ap.ArgumentParser(formatter_class=ap.RawTextHelpFormatter,
+                               description='''Make a run happen.''')
 
-    parser.add_argument('-r', '--run', action='store_true', help='Run the analysis.')
+    parser.add_argument('-r', '--run',
+                        action='store_true',
+                        help='Run the analysis.')
     args = parser.parse_args()
-    
+
     if args.run:
         run(dfile)
-
-
 
 
 def get_baseline_rmss(modelName, baselines=baselines):
     """Iterate through a range of baseline cutoffs and compare the results."""
     data_list = []
     for b in baselines:
-        print '\n\n\n'
-        print '    NEW ITERATION'
-        print 'Baseline: ', b
-        print '\n\n'
-
-	# Do a dirty clean to get the rms value to clean down to.
+        print '\n\n\n    NEW ITERATION\nBaseline: ', b, '\n\n'
+        name = modelName + str(b)
         print "Starting dirty clean"
-        icr(modelName, min_baseline=b, niters=1)
-        mean, rms = imstat(modelName + str(b))
-        print "Dirty rms is", rms
-	# Now do a real clean
-	mean, rms = imstat(modelName + str(b))
-	icr(modelName, min_baseline=b, rms=0.5*rms)
-        step_output = {'RMS': rms,
-                       'Mean': mean,
-                       'Baseline': b}
 
-        data_list.append(step_output)
+        # Check if we've already icr'ed this one.
+        if name in sp.check_output(['ls']):
+            mean, rms = imstat(name)
+
+        # If not, get rms, clean down to it.
+        else:
+            # do a dirty clean to get the rms value to clean down to.
+            icr(modelName, min_baseline=b, niters=1)
+            mean, rms = imstat(name)
+            print "Dirty rms is", rms
+            # Now do a real clean
+            mean, rms = imstat(name)
+            icr(modelName, min_baseline=b, rms=0.5*rms)
+
+            step_output = {'RMS': rms,
+                           'Mean': mean,
+                           'Baseline': b}
+
+            data_list.append(step_output)
 
     data_pd = pd.DataFrame(data_list)
     return data_pd
