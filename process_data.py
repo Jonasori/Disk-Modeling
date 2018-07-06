30a537711e1b43a5bb6418568a93dba443b6648b
Jonas Powell <jonasori@gmail.com>
2018-07-06 16:48:47 -0400

    b

--- process_data.py
+++ process_data.py
@@ -25,10 +25,10 @@ def pipe(commands):
 def process_data(mol, split_range, raw_data_path, final_data_path):
     """Cvel, split, and export as uvf the original cont-sub'ed .ms."""
     pipe(["cvel(",
-          "vis='{}/calibrated-{}.ms.contsub',".format(raw_data_path, mol),
+          "vis='{}calibrated-{}.ms.contsub',".format(raw_data_path, mol),
           "outputvis='{}_cvel.ms',".format(final_data_path),
-          "field=OrionField4',",
-          "restfreq={}GHz',".format(lines[mol]['restfreq']),
+          "field='OrionField4',",
+          "restfreq='{}GHz',".format(lines[mol]['restfreq']),
           "outframe='LSRK')"
           ])
 
