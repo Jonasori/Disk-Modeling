f9f4891a18b04d216d91029414b37d1e81ddddad
Jonas Powell <jonasori@gmail.com>
2018-07-06 23:54:10 -0400

    b

--- baseline_cutoff.py
+++ baseline_cutoff.py
@@ -56,10 +56,10 @@ def get_baseline_rmss(vis, baselines=baselines, remake_all=False,
     run_dir = 'baseline_' + mol + str(int(niters)) + '/'
     scratch_dir = '/scratch/jonas/baselines/' + run_dir
 
-    sp.call(['rm', '-rf', './baselines/{}'.format(run_dir)], shell=True)
-    sp.call(['rm', '-rf', scratch_dir], shell=True)
+    sp.call(['rm -rf {}'.format(scratch_dir)], shell=True)
+    sp.call(['rm -rf ./baselines/{}'.format(run_dir[:-1])], shell=True)
 
-    sp.call(['mkdir', scratch_dir], shell=True)
+    sp.call(['mkdir {}'.format(scratch_dir)], shell=True)
     sp.call(['ln', '-s', scratch_dir, './baselines/'])
 
     sp.call(['cp', '-r', '{}.vis'.format(vis),
