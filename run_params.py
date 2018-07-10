5e27a4046be35f22b54bd40850fcfa81204eac43
Jonas Powell <jonasori@gmail.com>
2018-07-10 12:14:56 -0400

    b

--- run_params.py
+++ run_params.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # Big run values
-
+"""
 TatmsA = np.arange(10, 300, 50)
 TqqA = -1 * np.arange(-1., 4, 0.7)
 XmolA = -1 * np.arange(3, 9, 1.)
@@ -22,14 +22,14 @@ XmolB = -1 * np.arange(3., 9., 1.)
 RAoutB = np.arange(20, 250, 60)
 PAB = np.array([135])
 InclB = np.array([45])
-
+"""
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Short test run values
 
-""""
-# Parameters for Disk A
 
+# Parameters for Disk A
+"""
 TatmsA = np.array([300])
 TqqA = -1 * np.array([-0.5])
 XmolA = -1 * np.array([6.])
@@ -48,7 +48,7 @@ InclB = np.array([30, 60])
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Medium-length test run values
-"""
+
 # Parameters for Disk B
 TatmsA = np.array([100, 300, 500])
 TqqA = -1 * np.array([-0.5, -0.7, -1.])
@@ -64,7 +64,7 @@ XmolB = -1 * np.array([7., 9.])
 RAoutB = np.array([200, 400])
 PAB = np.array([135])
 InclB = np.array([30])
-"""
+
 
 
 
