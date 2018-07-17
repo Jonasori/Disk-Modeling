93915c0d83b24e37ae8e5f5aa2630648fe28d4dc
Jonas Powell <jonasori@gmail.com>
2018-07-17 12:47:07 -0400

    b

--- run_params.py
+++ run_params.py
@@ -12,7 +12,7 @@ import numpy as np
 TatmsA = np.arange(10, 300, 50)
 TqqA = -1 * np.arange(-1., 4, 0.7)
 XmolA = -1 * np.arange(3, 9, 1.)
-RAoutA = np.arange(200, 400, 40)
+R_outA = np.arange(200, 400, 40)
 # PA and InclA are from Williams et al
 PAA = np.array([69.7])
 InclA = np.array([65])
@@ -21,7 +21,7 @@ InclA = np.array([65])
 TatmsB = np.arange(20, 240, 50)
 TqqB = -1 * np.arange(-1, 4., 0.7)
 XmolB = -1 * np.arange(3., 9., 1.)
-RAoutB = np.arange(20, 250, 60)
+R_outB = np.arange(20, 250, 60)
 PAB = np.array([135])
 InclB = np.array([45])
 """
@@ -30,11 +30,11 @@ InclB = np.array([45])
 # Short test run values
 
 # Parameters for Disk A
-"""
+
 TatmsA = np.array([300])
 TqqA = -1 * np.array([-0.5])
 XmolA = -1 * np.array([6.])
-RAoutA = np.array([300])
+R_outA = np.array([300])
 PAA = np.array([250])
 InclA = np.array([65, 70])
 
@@ -42,20 +42,20 @@ InclA = np.array([65, 70])
 TatmsB = np.array([200])
 TqqB = -1 * np.array([-0.6])
 XmolB = -1 * np.array([7.])
-RAoutB = np.array([200])
+R_outB = np.array([200])
 PAB = np.array([135])
 InclB = np.array([30, 60])
-"""
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # Medium-length test run values
-
+"""
 # Parameters for Disk B
 TatmsA = np.array([10, 75, 150, 300])
 # TqqA = -1 * np.array([-0.5, -0.7, -1.])
 TqqA = np.array([-1.1, -0.7, -0.1, 0.5])
 XmolA = -1 * np.array([6., 8., 10., 12.])
-RAoutA = np.array([50, 200, 400, 600])
+R_outA = np.array([50, 200, 400, 600])
 PAA = np.array([250])
 InclA = np.array([65])
 
@@ -64,18 +64,12 @@ TatmsB = np.array([1, 10, 75, 150])
 # TqqB = -1 * np.array([-0.6, -0.9])
 TqqB = np.array([-0.7])
 XmolB = -1 * np.array([4., 6., 8., 10.])
-RAoutB = np.array([50, 200, 400, 600])
+R_outB = np.array([50, 200, 400, 600])
 PAB = np.array([135])
 InclB = np.array([30])
+"""
 
 
 
-
-
-# Changed position angle B to have it move the right direction
-
-
-
-
-diskAParams = np.array([TatmsA, TqqA, XmolA, RAoutA, PAA, InclA])
-diskBParams = np.array([TatmsB, TqqB, XmolB, RAoutB, PAB, InclB])
+diskAParams = np.array([TatmsA, TqqA, XmolA, R_outA, PAA, InclA])
+diskBParams = np.array([TatmsB, TqqB, XmolB, R_outB, PAB, InclB])
